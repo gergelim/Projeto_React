da070609f76eed9cb557239241b8234dafbea0e2
André <andre.santos@ifes.edu.br>
2026-04-28 15:05:47 -0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import Card from "../components/Card";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+
     const artigos = [
         { id: 1, title: "breaking-news", data_publicacao: "2022" },
         { id: 2, title: "Ebola", data_publicacao: "2017" },
         { id: 3, title: "Habemus Papam", data_publicacao: "2025" },
-    ]
+    ];
 
     const handleSearch =   ( ) => {
-
-    }
+      alert(searchQuery)
+    };
 
     return (
     
@@ -19,8 +22,10 @@ function Home() {
           type="text" 
           placeholder ="Buscar pelo artigo..." 
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button></button>
+        <button type="submit" className="search-button">Search</button>
        </form>
 
       <div className="artigos-grid">
